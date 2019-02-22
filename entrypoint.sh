315a50d90a86ed9827428316b6145bb7841be225
Anatoliy <mirzuev.a.v@gmail.com>
2019-02-22 23:07:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 echo ${PATH}
 
 eval "$(rbenv init -)"
-rbenv global 2.5.0
+rbenv global ${RUBY_VERSION} 
+rbenv local ${RUBY_VERSION}
 rbenv rehash
 ruby -v
+gem install pg 
+gem install elasticsearch 
+gem install mongodb
 
